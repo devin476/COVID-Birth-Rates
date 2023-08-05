519b9020b76d0eb8dc95aeddb4e26ca9dd9398e2
Dev <110751047+devinstreeter@users.noreply.github.com>
2023-08-04 23:03:48 -0500

    significant speed increase to state loop. data is downloaded concurrently and append opperation replaced with concat method.

--- stateloop.py
+++ stateloop.py
@@ -1,17 +1,18 @@
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
 
-#Loops through USA States on Google open Data by location key
+#Loops through USA States on Google open Data by location key and downloads data concurrently 
 #Prints error and error code if iteration is failed
-def googleDataCall():
-    states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-    googleData = pd.DataFrame()
+def googleDataCall(state):
+    try:
+        url = f"https://storage.googleapis.com/covid19-open-data/v3/location/US_{state}.csv"
+        state_data = pd.read_csv(url)
+        return state_data
+    except Exception as e:
+        print(f"Failed to download data for state: {state}. Error: {e}")
+states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+with ThreadPoolExecutor() as executor:
+    googleDl = list(executor.map(googleDataCall, states))
+googleData = pd.concat(googleDl, ignore_index=True)
+print(googleData)
 
-    for state in states:
-        try:
-            url = f"https://storage.googleapis.com/covid19-open-data/v3/location/US_{state}.csv"
-            state_data = pd.read_csv(url)
-            googleData = googleData.append(state_data)
-        except Exception as e:
-            print(f"Failed to download data for state: {state}. Error: {e}")
-    googleData.reset_index(drop=True, inplace=True)
-    return(googleData)
